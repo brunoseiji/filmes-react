f318b4966e2864d267282a699fb3a3d47ccb3373
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2020-02-14 23:27:04 -0300

    adicionando api filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filmes: []
+        };
+
+        this.loadFilmes = this.loadFilmes.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadFilmes();
+    }
+
+    loadFilmes(){
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+        fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+            this.setState({filmes: json});
+            console.log(json);
+        });
+    
+      }
+
     render() {
         return(
             <div>
-                Página Home
+                
             </div>
         )
     }
